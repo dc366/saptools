36a6285935d818073e3d93f31f2194a5f31a9eb5
Andrew C. Polimeni <ACP@LERA.LOCAL>
2021-03-01 11:37:54 -0500

    added selectionPoints.py which does the TP point grouping

---
+++ newMeshPrep/selectPoints.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 09:41:24 2021
+
+@author: ACP
+"""
+#This script creates a group for the "TP" Points in the SAP Model########3
+
+
+
+import attach
+sapBox = attach.sapApplication()
+
+#Before running this script, create a group called "allJoints" and assign the joints or subgroup of joints that contain the TP Joints
+jointGroup = "allJoints"
+
+#Other Parameters:
+jointList = attach.sapGroup(sapBox, jointGroup).ObjectName
+
+tpJoints=[]
+for i in range(len(jointList)):
+    jointName= jointList[i]
+    if ("TP" in jointName):  #"TP" are the Joints at T-pin ends in Rick's naming convention
+        tpJoints.append(jointName)
+        
+sapBox.add_joints_to_group("tpJoints",tpJoints)
