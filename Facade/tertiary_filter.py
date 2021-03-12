3e7f0304ed7f626a1a0ac484b951ece27f97c2d2
David J. Chang <djc@LERA.LOCAL>
2021-03-12 11:58:40 -0500

    add WW to secondary members group

--- Facade/tertiary_filter.py
+++ Facade/tertiary_filter.py
@@ -13,7 +13,7 @@ import attach
 
 a = attach.sapApplication()
 
-tertiary_substrings = ['_KH_','_PB_','_PH_','_BB_']
+tertiary_substrings = ['_KH_','_PB_','_PH_','_BB_','spTert']
 tertiary_sections = ['CONN_rod','CONN_rod_1in']
 connSTIFF_sections = ['CONN_stiff']
 tertiary_members = set()
@@ -24,6 +24,9 @@ connSTIFF_members = set()
 [_,frames,_] = a.SapModel.FrameObj.GetNameList()
 frames_list = list(frames)
 
+#tertiary_members = [frame in frames if any(sub in frame for sub in tertiary_substrings)]
+#secondary_members = [frame in frames if not any(sub in frame for sub in tertiary_substrings)]
+
 for frame in frames:
     if any(sub in frame for sub in tertiary_substrings):
         tertiary_members.add(frame)
@@ -31,6 +34,19 @@ for frame in frames:
         secondary_members.add(frame)
 
 #search remaining unfiltered frames for sections that are tertiary and move them to another group
+
+""" List Comprehension alternatives, to be tested
+getsection = a.SapModel.FrameObj.GetSection
+frame_sections = [(frame,getsection(frame)) for frame in secondary_members]
+tertiary_section_check = [frame for (frame,section) in frame_sections if section in tertiary_sections]
+connSTIFF_section_check = [frame for (frame,section) in frame_sections if section in connSTIFF_sections]
+
+tertiary_members = tertiary_members | tertiary_section_check
+connSTIFF_members = connSTIFF_members | connSTIFF_section_check
+secondary_members = secondary_members - tertiary_section_check
+secondary_members = secondary_members - connSTIFF_section_check
+"""
+
 for frame in secondary_members.copy():
     frame_section,_,_ = a.SapModel.FrameObj.GetSection(frame)
     if frame_section in tertiary_sections:
@@ -45,5 +61,11 @@ a.add_frames_to_group('LERA_tertiary',tertiary_members)
 a.add_frames_to_group('LERA_secondary',secondary_members)
 a.add_frames_to_group('LERA_CONNstiff',connSTIFF_members)
 
+#add WW elements to secondary members group
+a.SapModel.SelectObj.PropertyArea("WW")
+[ww_elems,_] = a.get_list_sap("area")
+a.add_areas_to_group('LERA_secondary',ww_elems)
+
 #assign reverseSW to secondary members
-a.SapModel.FrameObj.SetLoadGravity("LERA_secondary","reverseSW",X=0,Y=0,Z=-1.0,ItemType=1)
+a.SapModel.FrameObj.SetLoadGravity("LERA_secondary","reverseSW",X=0,Y=0,Z=-1.0,Replace=True,ItemType=1)
+a.SapModel.AreaObj.SetLoadGravity("LERA_secondary","reverseSW",X=0,Y=0,Z=-1.0,Replace=True,ItemType=1)
