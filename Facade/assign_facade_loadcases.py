2de49d8566a78e66d2f8b57e730085c5f0b10b02
David J. Chang <djc@LERA.LOCAL>
2021-04-14 11:18:11 -0400

    Coordinate required load cases with ACP

--- Facade/assign_facade_loadcases.py
+++ Facade/assign_facade_loadcases.py
@@ -12,22 +12,56 @@ a = attach.sapApplication()
 
 a.SapModel.LoadPatterns.Add("reverseSW",8)
 
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT")
 a.SapModel.LoadCases.StaticNonlinear.SetCase("SERVICE-WIND(+)")
 a.SapModel.LoadCases.StaticNonlinear.SetCase("SERVICE-WIND(-)")
-a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD(+)SERVICE-WIND")
-a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD(-)SERVICE-WIND")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(+)SERVICE_WIND_1.0")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(-)SERVICE_WIND_1.0")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(+)SERVICE_WIND_0.7")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(-)SERVICE_WIND_0.7")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(+)SERVICE_WIND_1.0x0.78_ASCE7-16")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(-)SERVICE_WIND_1.0x0.78_ASCE7-16")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(+)SERVICE_WIND_0.7x0.78_ASCE7-16")
+a.SapModel.LoadCases.StaticNonlinear.SetCase("DEAD_ALT(-)SERVICE_WIND_0.7x0.78_ASCE7-16")
 
-a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD",2,
-                                              ["Load","Load"],
-                                              ["DEAD","reverseSW"],
-                                              [1.0,1.0])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT",3,
+                                              ["Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP"],
+                                              [1.0,1.0,1.0])
+
 a.SapModel.LoadCases.StaticNonlinear.SetLoads("SERVICE-WIND(+)",1,["Load"],["WIND(+)"],[1.0])
 a.SapModel.LoadCases.StaticNonlinear.SetLoads("SERVICE-WIND(-)",1,["Load"],["WIND(-)"],[1.0])
-a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD(+)SERVICE-WIND",3,
-                                              ["Load","Load","Load"],
-                                              ["DEAD","WIND(+)","reverseSW"],
-                                              [1.0,1.0,1.0])
-a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD(-)SERVICE-WIND",3,
-                                              ["Load","Load","Load"],
-                                              ["DEAD","WIND(-)","reverseSW"],
-                                              [1.0,1.0,1.0])
+
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(+)SERVICE_WIND_1.0",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(+)"],
+                                              [1.0,1.0,1.0,1.0])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(-)SERVICE_WIND_1.0",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(-)"],
+                                              [1.0,1.0,1.0,1.0])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(+)SERVICE_WIND_0.7",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(+)"],
+                                              [1.0,1.0,1.0,0.7])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(-)SERVICE_WIND_0.7",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(-)"],
+                                              [1.0,1.0,1.0,0.7])
+
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(+)SERVICE_WIND_1.0x0.78_ASCE7-16",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(+)"],
+                                              [1.0,1.0,1.0,0.78])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(-)SERVICE_WIND_1.0x0.78_ASCE7-16",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(-)"],
+                                              [1.0,1.0,1.0,0.78])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(+)SERVICE_WIND_0.7x0.78_ASCE7-16",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(+)"],
+                                              [1.0,1.0,1.0,0.546])
+a.SapModel.LoadCases.StaticNonlinear.SetLoads("DEAD_ALT(-)SERVICE_WIND_0.7x0.78_ASCE7-16",4,
+                                              ["Load","Load","Load","Load"],
+                                              ["DEAD","reverseSW","SDL_MEP","WIND(-)"],
+                                              [1.0,1.0,1.0,0.546])
